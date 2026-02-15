b9b55770270212e2300c0cf47cf274228b137c66
Josh (Castawhat) <castawaypalmtree@gmail.com>
2026-02-15 19:16:01 +1300

    no more errorsa?

--- welcome.js
+++ welcome.js
@@ -5,3 +5,7 @@ document.getElementById("startBtn")?.addEventListener("click", () => {
 document.getElementById("nextBtn")?.addEventListener("click", () => {
     window.location.href = "welcome3.html";
 });
+
+document.getElementById("doneBtn")?.addEventListener("click", () => {
+    window.location.href = "welcome4.html";
+});
